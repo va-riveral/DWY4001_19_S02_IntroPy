11647cf27e64fc52ced746dbe52b95f8f1b6cfe2
Valeska Rivera <va.riveral@alumnos.duoc.cl>
2019-09-24 10:17:22 -0300

    Agregamos el hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# La linea de arriba es para correrlo en linux 
+# En windows no es necesaria, pero yo suelo colocarla igual.
+
+#guardar en la carpeta del proyacto, con el nombre :
+# 'hola_mundo.py'
+
+ 
