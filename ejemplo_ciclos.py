d86fded2fef23e634df3cfc896c3b74096b6f824
Valeska Rivera <va.riveral@alumnos.duoc.cl>
2019-09-24 10:37:23 -0300

    Agregamos un ejemplo de uso de ciclos

---
+++ ejemplo_ciclos.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+# guardar como ejemplo_ciclos.py
+
+# primero un while .
+# notese los detalles, pues en python la indentecion 
+# es importante.
+
+i = 10 
+while i > 0:
+	print(i)
+	i = i - 1
+
+#ahora un for 
+regiones = ["Arica", "Tarapaca", "Antofagasta","Atacama"]
+for r in regiones: 
+	print(r)
